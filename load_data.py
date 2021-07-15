a3363aee74100f67b366fc96cd03d67d57c7f641
André Mathisen <9752808+anzure@users.noreply.github.com>
2021-07-15 12:33:44 +0200

    🎉 Retrieve valid tickers.

--- load_data.py
+++ load_data.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import numpy as np
+import sys as system
 
+# Connect to database
 database = mysql.connector.connect(
     host="odit-do-user-6851721-0.b.db.ondigitalocean.com",
     port=25060,
@@ -9,21 +11,32 @@ database = mysql.connector.connect(
     database="odit-market"
 )
 
+# Load all data
 cursor = database.cursor()
-cursor.execute("SELECT id FROM stock WHERE end_of_day=1 LIMIT 10")
+cursor.execute("SELECT symbol,high,low,open,close,volume,date FROM history LIMIT 500000")
 cursor = cursor.fetchall()
-stocks = cursor
+history_items = np.array(cursor)
+cursor = cursor.clear()
 
-#cursor = database.cursor()
-#cursor.execute("SELECT symbol,high,low,open,close,volume,date FROM history LIMIT 10")
-#cursor = cursor.fetchall()
+# Retrieve all tickers
+all_tickers = []
+for item in history_items:
+    symbol = item[0]
+    if not all_tickers.__contains__(symbol):
+        all_tickers.append(symbol)
 
+# Filter valid tickers
+valid_tickers = []
+for ticker in all_tickers:
+    count = 0
+    for item in history_items:
+        symbol = item[0]
+        if symbol == ticker:
+            count = count + 1
+    if count > 2000:
+        valid_tickers.append(ticker)
 
-
-for stock in stocks:
-    stock_id = stock[0]
-    cursor = database.cursor()
-    cursor.execute(f"SELECT COUNT(*) FROM history WHERE symbol='{stock_id}' LIMIT 1")
-    cursor = cursor.fetchall()
-    history_count = cursor[0][0]
-    print(f"{stock_id} has {history_count} items.")
+# Print result
+print(f"Valid tickers: {len(valid_tickers)}")
+print(f"Total tickers: {len(all_tickers)}")
+system.exit(0)
