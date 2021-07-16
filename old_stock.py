b02d86d0cd705f30a4bd77bbc67f393cdf7156cd
André Mathisen <9752808+anzure@users.noreply.github.com>
2021-07-16 16:12:58 +0200

    🎉 Further development.

--- old_stock.py
+++ old_stock.py
@@ -1,32 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import pandas_datareader as web
+import tensorflow as tf
 import datetime as dt
+import pickle
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
 # Load Data
-company = 'NVDA'
+company = "ATEA.XOSL"
 
-start = dt.datetime(2012, 1, 1)
-end = dt.datetime(2020, 1, 1)
+dataframe = pd.read_csv("dataset.csv", delimiter=";")
+dataframe = pd.read_pickle("dataset.pickle")
 
-data = web.DataReader(company, 'yahoo', start, end)
+data = dataframe[:2000]
+test_data = dataframe[2000:]
 
 # Prepare Data
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+scaled_data = scaler.fit_transform(data['close'].values.reshape(-1, 1))
 
 prediction_days = 60
+future_day = 100
 
 x_train = []
 y_train = []
 
-for x in range(prediction_days, len(scaled_data)):
+for x in range(prediction_days, len(scaled_data) - future_day):
     x_train.append(scaled_data[x - prediction_days:x, 0])
-    y_train.append(scaled_data[x, 0])
+    y_train.append(scaled_data[x + future_day, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
@@ -46,13 +49,9 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
 # Load Test Data
-test_start = dt.datetime(2020, 1, 1)
-test_end = dt.datetime.now()
+actual_prices = test_data['close'].values
 
-test_data = web.DataReader(company, 'yahoo', test_start, test_end)
-actual_prices = test_data['Close'].values
-
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+total_dataset = pd.concat((data['close'], test_data['close']), axis=0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
 model_inputs = model_inputs.reshape(-1, 1)
@@ -79,3 +78,12 @@ plt.xlabel("Time")
 plt.ylabel(f"{company} Share Price")
 plt.legend()
 plt.show()
+
+# Predict Next Day
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(prediction)
--- train_stock.py
+++ train_stock.py
@@ -1,3 +1,4 @@
+import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,83 +8,66 @@ import pickle
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
+from sklearn.preprocessing import RobustScaler
 
-# Load Data
-company = "ATEA.XOSL"
+df = pd.read_csv("dataset.csv", delimiter=";", parse_dates=["date"], index_col="date")
+df = df.drop(columns=['symbol'])
 
-dataframe = pd.read_csv("dataset.csv", delimiter=";")
-dataframe = pd.read_pickle("dataset.pickle")
+train_size = int(len(df) * 0.9)
+test_size = len(df) - train_size
+train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
+print(train.shape, test.shape)
 
-data = dataframe[:2000]
-test_data = dataframe[2000:]
+f_columns = ['high', 'low', 'open', 'volume']
+f_transformer = RobustScaler()
+cnt_transformer = RobustScaler()
 
-# Prepare Data
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data['close'].values.reshape(-1, 1))
+f_transformer = f_transformer.fit(train[f_columns].to_numpy())
+cnt_transformer = cnt_transformer.fit(train[['close']])
 
-prediction_days = 60
-future_day = 100
+train.loc[:, f_columns] = f_transformer.transform(train[f_columns].to_numpy())
+train['close'] = cnt_transformer.transform(train[['close']])
 
-x_train = []
-y_train = []
+test.loc[:, f_columns] = f_transformer.transform(test[f_columns].to_numpy())
+test['close'] = cnt_transformer.transform(test[['close']])
 
-for x in range(prediction_days, len(scaled_data) - future_day):
-    x_train.append(scaled_data[x - prediction_days:x, 0])
-    y_train.append(scaled_data[x + future_day, 0])
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+def create_dataset(X, y, time_steps=1):
+    Xs, ys = [], []
+    for i in range(len(X) - time_steps):
+        v = X.iloc[i: (i + time_steps)].to_numpy()
+        Xs.append(v)
+        ys.append(y.iloc[i + time_steps])
+    return np.array(Xs), np.array(ys)
 
-# Build The Model
-model = Sequential()
 
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
+TIME_STEPS = 90
+X_train, y_train = create_dataset(train, train.close, time_steps=TIME_STEPS)
+X_test, y_test = create_dataset(test, test.close, time_steps=TIME_STEPS)
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+# [samples, time_steps, n_features]
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
 
-# Load Test Data
-actual_prices = test_data['close'].values
+model = keras.Sequential()
+model.add(
+    keras.layers.Bidirectional(
+        keras.layers.LSTM(
+            units=128,
+            input_shape=(X_train.shape[1], X_train.shape[2])
+        )
+    )
+)
+model.add(keras.layers.Dropout(rate=0.2))
+model.add(keras.layers.Dense(units=1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+history = model.fit(
+    X_train, y_train,
+    epochs=30,
+    batch_size=32,
+    validation_split=0.1,
+    shuffle=False
+)
 
-total_dataset = pd.concat((data['close'], test_data['close']), axis=0)
-
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
-model_inputs = model_inputs.reshape(-1, 1)
-model_inputs = scaler.transform(model_inputs)
-
-# Make Predictions on Test Data
-
-x_test = []
-
-for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x - prediction_days:x, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-predicted_prices = model.predict(x_test)
-predicted_prices = scaler.inverse_transform(predicted_prices)
-
-# Plot The Test Predictions
-plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
-plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
-plt.title(f"{company} Share Price")
-plt.xlabel("Time")
-plt.ylabel(f"{company} Share Price")
-plt.legend()
-plt.show()
-
-# Predict Next Day
-real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
-real_data = np.array(real_data)
-real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-
-prediction = model.predict(real_data)
-prediction = scaler.inverse_transform(prediction)
-print(prediction)
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
