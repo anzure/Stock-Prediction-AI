a59a0e54b6e9b5b67d6218e282e88bab4660c954
André Mathisen <9752808+anzure@users.noreply.github.com>
2021-07-16 11:37:04 +0200

    🚀 Predicting stock price.

--- train_stock.py
+++ train_stock.py
@@ -1,28 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import pandas_datareader as web
+import tensorflow as tf
 import datetime as dt
+import pickle
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-crypto_currency = 'BTC'
-fiat_currency = 'USD'
+# Load Data
+company = "ATEA.XOSL"
 
-start = dt.datetime(2016, 1, 1)
-end = dt.datetime.now()
+dataframe = pd.read_csv("dataset.csv", delimiter=";")
+dataframe = pd.read_pickle("dataset.pickle")
 
-data = web.DataReader(f"{crypto_currency}-{fiat_currency}", "yahoo", start, end)
+data = dataframe[:2000]
+test_data = dataframe[2000:]
 
 # Prepare Data
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+scaled_data = scaler.fit_transform(data['close'].values.reshape(-1, 1))
 
 prediction_days = 60
-future_day = 30
+future_day = 100
 
-x_train, y_train = [], []
+x_train = []
+y_train = []
 
 for x in range(prediction_days, len(scaled_data) - future_day):
     x_train.append(scaled_data[x - prediction_days:x, 0])
@@ -31,8 +34,7 @@ for x in range(prediction_days, len(scaled_data) - future_day):
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-# Create Neural Network
-
+# Build The Model
 model = Sequential()
 
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
@@ -46,19 +48,16 @@ model.add(Dense(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
-# Testing The Model
+# Load Test Data
+actual_prices = test_data['close'].values
 
-test_start = dt.datetime(2020, 1, 1)
-test_end = dt.datetime.now()
-
-test_data = web.DataReader(f"{crypto_currency}-{fiat_currency}", "yahoo", test_start, test_end)
-actual_prices = test_data['Close'].values
-
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+total_dataset = pd.concat((data['close'], test_data['close']), axis=0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
 model_inputs = model_inputs.reshape(-1, 1)
-model_inputs = scaler.fit_transform(model_inputs)
+model_inputs = scaler.transform(model_inputs)
+
+# Make Predictions on Test Data
 
 x_test = []
 
@@ -68,19 +67,19 @@ for x in range(prediction_days, len(model_inputs)):
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-prediction_prices = model.predict(x_test)
-prediction_prices = scaler.inverse_transform(prediction_prices)
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
 
-plt.plot(actual_prices, color="black", label="Actual Prices")
-plt.plot(prediction_prices, color="green", label="Predicted Prices")
-plt.title(f"{crypto_currency} price prediction")
+# Plot The Test Predictions
+plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
+plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
+plt.title(f"{company} Share Price")
 plt.xlabel("Time")
-plt.ylabel("Price")
-plt.legend(loc="upper left")
+plt.ylabel(f"{company} Share Price")
+plt.legend()
 plt.show()
 
 # Predict Next Day
-
 real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
